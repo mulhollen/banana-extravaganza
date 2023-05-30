06e9bf0b6ebb1a27bbabb79eebdbdf5798899959
mulhollen <lindsaymulhollen@gmail.com>
2023-05-30 14:39:23 -0500

    h1 update

--- src/components/shared-components/wave.tsx
+++ src/components/shared-components/wave.tsx
@@ -9,7 +9,9 @@ export default function Wave() {
         <div className='wave d-flex flex-column justify-content-center pt-5 display-4'>
           <Image src={waveBlockTop} className='vw-100' alt='' />
           <div className=' d-flex justify-content-center h-auto' style={{backgroundColor: '#A1CDF4'}}>
-            <h1 className='bodyWidth h-auto m-0 px-5 text-align-center'>Lindsay Mulhollen is a dev manager, front-end developer, and artist with a love of user experience and accessibility.</h1>
+            <h1 className='bodyWidth h-auto m-0 px-5 text-align-center'>
+              Lindsay Mulhollen is a dev manager, front-end developer, and artist with a fascination for how humans interact with technology as users and builders.
+              </h1>
           </div>
           <Image src={waveBlockBottom} className='vw-100' alt='' />
         </div>
