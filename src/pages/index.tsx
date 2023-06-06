4f49eba22183204207c7c477f2c1c2048b920050
mulhollen <lindsaymulhollen@gmail.com>
2023-06-06 13:07:54 -0500

    build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Hero from '@/components/shared-components/hero'
 import MinimalPortfolio from '@/components/shared-components/minimal-portfolio'
 import Wave from '@/components/shared-components/wave'
 
-export default function Home() {
+export default function Home():JSX.Element {
 
   return (
     <div>
